c05b9259a00b127e71708259c387da2e85817c2e
Lily Geidelberg <eg1012@dide.local>
2020-11-04 15:10:14 +0000

    updated R0

--- R/R0.R
+++ R/R0.R
@@ -2,7 +2,7 @@ invisible('
 Compute R0 from parameter posteriors
 ') 
 
-X <- readRDS( 'combinedLog.rds' )
+X <- readRDS( 'data/logs.rds' )
 gamma1 = 96
 tau = 74
 ph = 0.20
@@ -14,37 +14,5 @@ Rs = b2R( X$seir.b )
 print( summary( Rs ))
 print( quantile( Rs  ,  c(.025, .975) )  )
 
-invisible(
-'
-ph 20pc: 
-   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-  1.463   1.644   1.846   1.986   2.217   5.447 
-    2.5%    97.5% 
-1.482970 3.146523 
 
 
-> print( summary( Rs ))
-   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-  1.018   1.144   1.285   1.382   1.543   3.791 
-> print( quantile( Rs  ,  c(.025, .975) )  )
-    2.5%    97.5% 
-1.032099 2.189878 
-
-'
-)
-
-if (FALSE)
-{ # run interactively : 
-#~ work out what k would be given these parameters 
-gamma1 = 96
-tau = 74
-#~ ph = 0.1350287
-ph = 0.20
-R = 2 
-#~ R = (1-ph) * Rl + ph * tau * Rl -->
-Rl = R / ( (1-ph ) + ph * tau  )
-v = ( (1-ph)*(Rl+Rl^2) + ph*(tau*Rl + tau^2*Rl^2) )
-#~ v = R + R^2 / k -->
-k = R^2 / (v - R)
-#~ 22%
-}
